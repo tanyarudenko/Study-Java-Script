29d1afafb2401295277d6ec7045928cce694efdf
Tetiana <ru8enetka@meta.ua>
2026-04-21 09:22:25 +0300

    18 lectures

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt ("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt ("Один из последних просмотренных фильмов?", ""),
+      b = prompt ("На сколько оцените его?", ""),
+      c = prompt ("Один из последних просмотренных фильмов?", ""),
+      d = prompt ("На сколько оцените его?", "");
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+
+console.log(personalMovieDb);
